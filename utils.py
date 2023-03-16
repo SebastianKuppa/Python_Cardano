17b5e35787cad75f3bbbbd416678da037b015569
sk <sebastian.kuppa@web.de>
2023-03-16 14:44:30 +0800

    refactoring

--- utils.py
+++ utils.py
@@ -1,6 +1,24 @@
 import pycardano
 
 
+def load_keys(signing_key_path="./keys/payment.skey", verification_key_path="./keys/payment.vkey"):
+    payment_signing_key = pycardano.PaymentSigningKey.generate()
+    payment_signing_key = payment_signing_key.load(signing_key_path)
+    payment_verification_key = pycardano.PaymentVerificationKey.from_signing_key(payment_signing_key)
+    payment_verification_key = payment_verification_key.load(verification_key_path)
+
+    return payment_signing_key, payment_verification_key
+
+
+def save_keys(signing_key_path="./keys/payment.skey", verification_key_path="./keys/payment.vkey"):
+    payment_signing_key = pycardano.PaymentSigningKey.generate()
+    payment_signing_key = payment_signing_key.save(signing_key_path)
+    payment_verification_key = pycardano.PaymentVerificationKey.from_signing_key(payment_signing_key)
+    payment_verification_key = payment_verification_key.save(verification_key_path)
+
+    return payment_signing_key, payment_verification_key
+
+
 def create_address():
     payment_signing_key = pycardano.PaymentSigningKey.generate()
     payment_signing_key.save("./keys/payment.skey")
@@ -13,11 +31,8 @@ def create_address():
 
 
 def get_address(signing_key_path="./keys/payment.skey", verification_key_path="./keys/payment.vkey"):
-    payment_signing_key = pycardano.PaymentSigningKey.generate()
-    payment_signing_key = payment_signing_key.load(signing_key_path)
-    payment_verification_key = pycardano.PaymentVerificationKey.from_signing_key(payment_signing_key)
-    payment_verification_key = payment_verification_key.load(verification_key_path)
-
+    _, payment_verification_key = load_keys(signing_key_path=signing_key_path,
+                                            verification_key_path=verification_key_path)
     network = pycardano.Network.TESTNET
     address = pycardano.Address(payment_part=payment_verification_key.hash(), network=network)
     print(f'Address: {address}')
