3d3b7a4554dc9f86efc2731f76859aab6223ef60
sk <sebastian.kuppa@web.de>
2023-03-16 14:39:59 +0800

    added comments

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import utils
 
 
 if __name__ == '__main__':
+    # if no key files exists in folder ./keys yet, create them
     # utils.create_address()
+
+    # print the address of keys in ./keys folder
     utils.get_address()
 
