af528f5282021a8955c4b25c5a290b3f286271f8
sk <sebastian.kuppa@web.de>
2023-03-16 14:38:53 +0800

    added get_address()

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ import utils
 
 
 if __name__ == '__main__':
-    utils.create_address()
+    # utils.create_address()
+    utils.get_address()
+
--- utils.py
+++ utils.py
@@ -10,3 +10,15 @@ def create_address():
     network = pycardano.Network.TESTNET
     address = pycardano.Address(payment_part=payment_verification_key.hash(), network=network)
     print(f'Created address: {address}')
+
+
+def get_address(signing_key_path="./keys/payment.skey", verification_key_path="./keys/payment.vkey"):
+    payment_signing_key = pycardano.PaymentSigningKey.generate()
+    payment_signing_key = payment_signing_key.load(signing_key_path)
+    payment_verification_key = pycardano.PaymentVerificationKey.from_signing_key(payment_signing_key)
+    payment_verification_key = payment_verification_key.load(verification_key_path)
+
+    network = pycardano.Network.TESTNET
+    address = pycardano.Address(payment_part=payment_verification_key.hash(), network=network)
+    print(f'Address: {address}')
+
