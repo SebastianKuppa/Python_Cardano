34fab96d9dc5840c977de2d25bad5ed382ab96b5
sk <sebastian.kuppa@web.de>
2023-03-30 14:28:09 +0800

    added create_script_address() method

--- gift.py
+++ gift.py
@@ -1,4 +1,6 @@
 from opshin.prelude import *
+from pycardano import PlutusV2Script, plutus_script_hash, Network, Address
+
 
 @dataclass()
 class CancelDatum(PlutusData):
@@ -11,3 +13,14 @@ def validator(datum: CancelDatum, redeemer: None, context: ScriptContext) -> Non
         if datum.pubkeyhash == s:
             sig_present = True
     assert sig_present
+
+
+def create_script_address(cbor_file="./build/gift/script.cbor"):
+    with open(cbor_file, "r") as f:
+        script_hex = f.read()
+    gift_script = PlutusV2Script(bytes.fromhex(script_hex))
+    script_hash = plutus_script_hash(gift_script)
+    network = Network.TESTNET
+    script_address = Address(script_hash, network=network)
+
+    return script_address
