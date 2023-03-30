0c64342dbb08a344c557da7375f6d3f7ecf99977
sk <sebastian.kuppa@web.de>
2023-03-30 16:53:21 +0800

    WIP

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ if __name__ == '__main__':
     # utils.create_address()
 
     # print the address of keys in ./keys folder
-    testnet_address = utils.get_address(signing_key_path="./keys/taker/payment.skey",
-                                        verification_key_path="./keys/taker/payment.vkey")
+    testnet_address = utils.get_address(signing_key_path="./keys/giver/payment.skey",
+                                        verification_key_path="./keys/giver/payment.vkey")
     print(testnet_address)
-    # amount_lovelaces = utils.get_lovelace_amount_from_address(testnet_address)
+    amount_lovelaces = utils.get_lovelace_amount_from_address(testnet_address)
     # utils.build_transaction(testnet_address)
     utils.datum_to_cbor()
