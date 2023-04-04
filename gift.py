bc58af6313483dc2f7e3c9e500c37c8caf90257f
sk <sebastian.kuppa@web.de>
2023-04-04 14:54:15 +0800

    create transaction, added comments

--- gift.py
+++ gift.py
@@ -1,3 +1,4 @@
+import pycardano
 from opshin.prelude import *
 from pycardano import PlutusV2Script, plutus_script_hash, Network, Address
 
@@ -31,8 +32,14 @@ def create_script_address(cbor_file="./build/gift/script.cbor"):
 
 
 if __name__ == "__main__":
+    # create smart contract address
     gift_script_address = create_script_address()
+    # load giver and receiver addresses
     giver_address = utils.get_address(signing_key_path="./keys/giver/payment.skey",
                                       verification_key_path="./keys/giver/payment.vkey")
     receiver_address = utils.get_address(signing_key_path="./keys/taker/payment.skey",
-                                      verification_key_path="./keys/taker/payment.vkey")
+                                         verification_key_path="./keys/taker/payment.vkey")
+    # build transaction
+    builder = pycardano.TransactionBuilder(context=utils.GLOBAL_context)
+    # add giver address as transaction input
+    builder.add_input_address(giver_address)
