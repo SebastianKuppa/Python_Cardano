b8c09011734d7e4b1197ac5b57cf8195daa9af6b
sk <sebastian.kuppa@web.de>
2023-04-07 14:15:27 +0800

    code cleanup

--- utils.py
+++ utils.py
@@ -60,20 +60,14 @@ def calc_ada_from_lovelace(amount_lovelace):
     return float(amount_lovelace)/1000000
 
 
-def get_address_utxos(address):
-    # get utxos for address
-    return GLOBAL_context.api.address_utxos(address)
-
-
 def get_lovelace_amount_from_address(address):
     # get utxos for address
-    utxos = get_address_utxos(address)
+    utxos = GLOBAL_context.utxos(address)
 
     # loop over all utxos and add the quantity of lovelace together
     amount_lovelace = 0
     for utxo in utxos:
         amount_lovelace += int(utxo.amount[0].quantity)
-    # print(f"The address: {address} has currently {calc_ada_from_lovelace(amount_lovelace)} ADA ({amount_lovelace} lovelace).")
 
     return amount_lovelace
 
@@ -93,7 +87,6 @@ def simple_send_transaction(input_address, output_address, send_amount=100_000_0
     # add input address as transaction input
     builder.add_input_address(address=input_address)
     builder.add_output(TransactionOutput.from_primitive([str(output_address), send_amount]))
-
     # ttl = time to live
     # TTL = slot + N slots. Where N is the amount of slots you want to
     # add to give the transaction a window to be included in a block.
@@ -124,4 +117,5 @@ def datum_to_cbor():
     datum = Sample_datum(123, b"1234", pycardano.IndefiniteList([4, 5, 6]), {1: b"1", 2: b"2"})
     datum_as_cbor = datum.to_cbor()
     print(f"datum as cbor: {datum_as_cbor}")
+    return datum_as_cbor
 
